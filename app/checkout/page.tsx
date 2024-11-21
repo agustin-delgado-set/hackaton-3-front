4d36e19868060f7f9822a426e4260d8b0b2f44c0
Agustin Delgado <agustindelgado@setandforget.io>
2024-11-21 17:15:05 -0300

    added special effects

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -9,6 +9,7 @@ import { useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import { useState } from "react";
 import { toast } from "sonner";
+import confetti from "canvas-confetti";
 
 export default function CheckoutPage() {
   const dispatch = useAppDispatch();
@@ -27,6 +28,35 @@ export default function CheckoutPage() {
       user_id: userId
     }));
   }
+
+  const handleConfetti = () => {
+    const duration = 5 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    const randomInRange = (min: number, max: number) =>
+      Math.random() * (max - min) + min;
+
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 250);
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
@@ -69,7 +99,10 @@ export default function CheckoutPage() {
           </div>
         </CardFooter>
         <Button
-          onClick={handleCreateOrder}
+          onClick={() => {
+            handleCreateOrder();
+            handleConfetti();
+          }}
           disabled={loading}
           className="w-full mt-4"
         >
